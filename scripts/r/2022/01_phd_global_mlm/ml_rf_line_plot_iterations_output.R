4922f60c3aae0fe1c1fc1b473b631695e94c22a3
Pallav Kumar Shrestha <pallav3.14@gmail.com>
2022-07-27 17:10:17 +0200

    Added - line plot of iterations output for ml_rf

---
+++ scripts/r/2022/01_phd_global_mlm/ml_rf_line_plot_iterations_output.R
@@ -0,0 +1,46 @@
+
+### OUTPUT VISUALIZATION: RANDOM FOREST
+###
+### Author:     Pallav Kumar Shrestha
+### Date:       27.07.2022
+### Licence:    CC BY 4.0
+
+
+
+
+path_d = "~/work/projects/09_phd/03_sim/02_randomforest/exp/3195/predictor_set_1_1000iterations/"
+
+data_raw = read.table(paste(path_d, file = "iteration_output.txt", sep = "/"))
+data_pro = data_raw[c(2, 5, 6)]
+colnames(data_pro) = c("iteration", "kge_t", "kge_v")           
+
+# optimal value time series with others as NA
+data_pro$kge_v_opt = data_pro$kge_v
+data_pro$kge_v_opt[data_pro$kge_v != max(data_pro$kge_v)] = NA
+data_pro$kge_t_opt = NA
+data_pro$kge_t_opt[which.max(data_pro$kge_v)] = data_pro$kge_t[which.max(data_pro$kge_v)]
+
+# ----------
+# PLOT
+# ----------
+
+# TRUE to enable things to be drawn outside the plot region
+par(xpd=FALSE) 
+
+# plot
+plot(data_pro$kge_t, ylim = c(0, 1), xlab = "iterations random forest (with random ordering of years)", ylab = "KGE", col = "blue")
+title("RANDOM FOREST: Rappbode dam outflow\n", adj = 0, cex.main = 1.2)
+title("\n\nPerformance of training and validation", adj = 0, cex.main = 1)
+points(data_pro$kge_v, col = "red")
+points(data_pro$kge_t_opt, col = "black", lwd = 4, bg = "blue", type = "p", pch = 21, cex = 2)
+points(data_pro$kge_v_opt, col = "black", lwd = 4, bg = "red", type = "p", pch = 21, cex = 2)
+abline(a = 0.84, b = 0, lwd = 3, lty = 2, col = "black") # chronology maintained training performance
+abline(a = 0, b = 0, lwd = 3, lty = 1, col = "black") # chronology maintained validation performance
+
+# add legend
+legend("topright", inset = c(0, 0), 
+       legend = c("training (random)", "validation (random)", "training (chronology)", "validation (chronology)"), 
+       pch = c(21, 21, NA, NA), lty = c(NA, NA, 2, 1), lwd = c(NA, NA, 3, 3),  col = c("blue", "red", "black", "black"),
+       ncol = 2)
+# legend(600, 1, legend = c("training", "validation"), pch = c(21, 21), col = c("blue", "red"),
+#        ncol = 2)
